d98390ef35f9b0b1baffd1d7776e1cb0f0bdf97f
david <davidealvarez1@gmail.com>
2021-04-05 20:12:02 -0700

    converted to use functional component and useState

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,57 @@
-// import './App.css';
-
 // function App() {
 //   return (
 //     <div className="App">
 //       <h1>Hello World!</h1>
+//       <Person name={"david"} age={"33"}/>
 //     </div>
 //   );
 // }
 
 // export default App;
 
-import React, {Component} from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import Person from './Person/Person';
+import './App.css';
 
-class App extends Component{
-  render(){
+//import {Component}
+//class App extends Component
+//render()
+const App = props => {
+    const [personsState,setPersonsState] = useState({
+      persons: [
+        {name: "max", age: 30},
+        {name: "david", age: 33},
+        {name: "chris", age: 38}
+      ],
+      otherState: 'some other value 2'
+    });
+
+    const [otherState, setOtherState] = useState('jhjhjhjhjh');
+
+    console.log("---->", personsState, otherState)
+
     return(
       <div className="App">
-        <h1>Hello World!</h1>
-        <Person name="david" age="33"/>
-        <Person>i am a props child</Person>
+        <h1>App.js</h1>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     )
-  }
 }
 
+
 export default App;
+
+
+
+// switchNameHandler = () =>  {
+//   //console.log("was clicked!")
+//   // NO BUENO->this.state.persons[0].name = "Maximillian";
+//   this.setState({persons:[
+//     {name: "maxibillian", age: 44},
+//     {name: "david a", age: 34},
+//     {name: "chris", age: 38}
+//   ]
+// })
+// }
