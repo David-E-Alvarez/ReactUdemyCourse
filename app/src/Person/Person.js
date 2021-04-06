adc715a89a784015706ffa83c7eb5794365c87d4
david <davidealvarez1@gmail.com>
2021-04-05 20:11:36 -0700

    deleted props.children line

--- app/src/Person/Person.js
+++ app/src/Person/Person.js
@@ -3,10 +3,9 @@ import React from 'react';
 const person = (props) => {//ES6
     return(
         <>
-        {console.log("props: ", props)}
+        {console.log("props---->: ", props)}
         <div>
             <p>Hi! My name is {props.name} and I'm {props.age} years old.</p>
-            <p>{props.children}</p>
         </div>
         </>
     )
